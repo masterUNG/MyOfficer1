12212ed83b3e6999cf6ec426200e55f6812d0a22
masterUNG <phrombutr@gmail.com>
2017-01-14 10:50:08 +0700

    Intent to SignUp Success

--- app/src/main/java/appewtc/masterung/myofficer/MainActivity.java
+++ app/src/main/java/appewtc/masterung/myofficer/MainActivity.java
@@ -1,13 +1,44 @@
 package appewtc.masterung.myofficer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit
+    private Button signInButton, signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+
+        //Button Controller
+        signInButton.setOnClickListener(MainActivity.this);
+        signUpButton.setOnClickListener(MainActivity.this);
+
+
+    }   // Main Method
+
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.button:   // for SignIn
+
+                break;
+            case R.id.button2:  // for SignUp
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+                break;
+        }
+
     }
-}
+}   // Main Class
