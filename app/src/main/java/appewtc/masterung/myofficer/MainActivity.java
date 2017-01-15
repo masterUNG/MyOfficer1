3d18abe1c1c6a568e42c6a0fced846281027f155
masterUNG <phrombutr@gmail.com>
2017-01-15 13:54:31 +0700

    Authen Success

--- app/src/main/java/appewtc/masterung/myofficer/MainActivity.java
+++ app/src/main/java/appewtc/masterung/myofficer/MainActivity.java
@@ -8,12 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Explicit
     private Button signInButton, signUpButton;
     private EditText userEditText , passwordEditText;
-    private String userString, passwordString;
+    private String userString, passwordString, truePasswordString;
 
 
     @Override
@@ -64,6 +67,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             myAlert.errorDialog("มีช่องว่าง", "กรุณากรอกทุกช่องสิคะ");
         } else if (checkUser()) {
             //User False
+            MyAlert myAlert = new MyAlert(MainActivity.this);
+            myAlert.errorDialog("User False", "No This User in my Database");
+        } else if (!passwordString.equals(truePasswordString)) {
+            //Password False
+            MyAlert myAlert = new MyAlert(MainActivity.this);
+            myAlert.errorDialog("Password False", "Please Try Again Password False");
+        } else {
+            //Password True
+
         }
 
     }   // myAuthen
@@ -79,10 +91,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String strJSON = synUser.get();
             Log.d("15janV1", "JSON ==> " + strJSON);
 
+            JSONArray jsonArray = new JSONArray(strJSON);
+            for (int i=0;i<jsonArray.length();i++) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                if (userString.equals(jsonObject.getString("User"))) {
+                    truePasswordString = jsonObject.getString("Password");
+                    result = false; // User True
+                }
+
+            }   // for
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-
 
         return result;
     }
